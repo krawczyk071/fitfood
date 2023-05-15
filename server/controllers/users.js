0543cd38dfcdad79de88c1a043fc8c61c5b235db
krawczyk071 <krawczyk071@gmail.com>
2023-05-15 12:32:48 +0200

    protected server route

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -14,9 +14,32 @@ export const addUser = async (req, res) => {
       return res.status(400).json({ message: "Username already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
-    res.json({ message: "User registered successfully" });
+
+    //       // Create user
+    //   const user = await User.create({
+    //     username,
+    //     password: hashedPassword,
+    //   })
+
+    //   if (user) {
+    //     res.status(201).json({
+    //       _id: user.id,
+    //       username: user.username,
+    //       token: generateToken(user._id),
+    //     })
+    //   } else {
+    //     res.status(400)
+    //     throw new Error('Invalid user data')
+    //   }
+    // })
+    res.status(201).json({
+      userID: newUser._id,
+      username: newUser.username,
+      token: generateToken(newUser._id),
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -24,6 +47,11 @@ export const addUser = async (req, res) => {
 
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({ message: "Please fill all the fields" });
+  }
+
   const user = await User.findOne({ username });
 
   if (!user) {
@@ -37,6 +65,18 @@ export const loginUser = async (req, res) => {
       .status(400)
       .json({ message: "Username or password is incorrect" });
   }
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  const token = generateToken(user._id);
   res.json({ token, userID: user._id });
+};
+
+export const getMe = async (req, res) => {
+  res.status(200).json(req.user);
+};
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addUser, loginUser } from "../controllers/users.js";
+import { addUser, getMe, loginUser } from "../controllers/users.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", addUser);
 router.post("/login", loginUser);
+router.get("/me", protect, getMe);
 
 export default router;
