ab32faf1b783c28e239a0dc770492c98a769c35c
krawczyk071 <krawczyk071@gmail.com>
2023-05-19 13:48:02 +0200

    adding to menu fixed

--- client/src/components/AddToMenuBtn.js
+++ client/src/components/AddToMenuBtn.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { toast } from "react-toastify";
-import { addToMenu } from "../redux/features/menuSlice";
-import { useDispatch } from "react-redux";
+import { addToMenu, delFromMenu } from "../redux/features/menuSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const AddToMenuBtn = ({ recipeId }) => {
   const dispatch = useDispatch();
+  const { data: menu } = useSelector((state) => state.menu);
 
-  async function onSubmit(e) {
+  async function onAdd(e) {
     await dispatch(addToMenu(recipeId));
     toast.success("Added");
   }
+  async function onDel(e) {
+    const menuId = menu.find((m) => m.recipeId === recipeId)._id;
+    try {
+      dispatch(delFromMenu(menuId));
+      toast.success("Meal deleted");
+    } catch (err) {
+      toast.error(err.message);
+    }
+  }
+
+  const isAdded = menu.some((m) => m.recipeId === recipeId);
   return (
     <>
-      <button className="btn" onClick={onSubmit}>
-        Save to MyMenu
-      </button>
+      {!isAdded ? (
+        <button className="btn" onClick={onAdd}>
+          Save to MyMenu
+        </button>
+      ) : (
+        <button className="btn" onClick={onDel}>
+          Remove
+        </button>
+      )}
     </>
   );
 };
